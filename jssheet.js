dddaeb8ffb4462fd4efeca2d935abcbe544be08b
Roberto <roberto.sousacordeiro@gmail.com>
2017-01-07 22:36:53 -0200

    Add files via upload

--- jssheet.js
+++ jssheet.js
@@ -1,6 +1,11 @@
-document.ready(function activaTab(tab){
-	$('.nav-tabs a').click(function(){
-    $(this).tab('show');
-  });
-};
-
+$(document).ready(function () {
+	    /*$('[data-spy="scroll"]').each(function () {
+                var $spy = $(this).scrollspy('refresh')
+            });
+        $('#target_nav').on('activate.bs.scrollspy', function(){
+           var activeTab = $('.nav-tabs li.active a');
+           activeTab.parent().removeClass('active');
+           activeTab.tab('show');
+           });*/
+		$('.nav a:contains("#portfolio")').addClass('active');
+        });
